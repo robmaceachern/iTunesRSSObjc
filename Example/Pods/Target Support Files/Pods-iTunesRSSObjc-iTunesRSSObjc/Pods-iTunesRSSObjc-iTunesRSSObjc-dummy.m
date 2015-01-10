341caea9aeb682acc79a0f996f20228d15c7261d
Rob MacEachern <rob@robmaceachern.com>
2015-01-09 23:53:42 -0800

    pod lib create iTunesRSSObjc

---
+++ Example/Pods/Target Support Files/Pods-iTunesRSSObjc-iTunesRSSObjc/Pods-iTunesRSSObjc-iTunesRSSObjc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTunesRSSObjc_iTunesRSSObjc : NSObject
+@end
+@implementation PodsDummy_Pods_iTunesRSSObjc_iTunesRSSObjc
+@end
---
+++ Example/Pods/Target Support Files/Pods-iTunesRSSObjc/Pods-iTunesRSSObjc-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// iTunesRSSObjc
+#define COCOAPODS_POD_AVAILABLE_iTunesRSSObjc
+#define COCOAPODS_VERSION_MAJOR_iTunesRSSObjc 0
+#define COCOAPODS_VERSION_MINOR_iTunesRSSObjc 1
+#define COCOAPODS_VERSION_PATCH_iTunesRSSObjc 0
+
